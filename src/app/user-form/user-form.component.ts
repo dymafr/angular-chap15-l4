4123f7300899fe704377e47bde10d42cab745b2f
dymafr <erwan1309@hotmail.com>
2021-01-05 15:11:04 +0100

    Initial commit

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { User } from "../user.interface";
+
+@Component({
+  selector: "app-user-form",
+  templateUrl: "./user-form.component.html",
+  styleUrls: ["./user-form.component.scss"]
+})
+export class UserFormComponent implements OnInit {
+  public userForm: FormGroup;
+  public user: User;
+
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.initForm();
+    });
+  }
+
+  initForm(user = { username: null, age: null }) {
+    this.userForm = this.fb.group({
+      username: [user.username],
+      age: [user.age]
+    });
+  }
+
+  submit() {
+    this.httpService
+      .post<User>("https://restapi.fr/api/angularuser", this.userForm.value)
+      .subscribe((user: User) => this.router.navigateByUrl("/"));
+  }
+}
